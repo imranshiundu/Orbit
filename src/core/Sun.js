66039aeae39d381c969d41ee5e3599c22eaf5653
imranshiundu <imranshiundu@gmail.com>
2026-06-23 09:40:55 +0300

    feat(core): apply texture to sun and remove tint

--- src/core/Sun.js
+++ src/core/Sun.js
@@ -10,6 +10,13 @@ export class Sun {
   init(scene) {
     const geometry = new THREE.SphereGeometry(20, 32, 32);
     const material = new THREE.MeshBasicMaterial({ color: 0xF5A623 });
+    
+    import('./TextureLoader.js').then(({ textureLoader }) => {
+      material.map = textureLoader.load('/textures/2k_sun.jpg');
+      material.color.setHex(0xffffff); // remove tint
+      material.needsUpdate = true;
+    });
+
     this.mesh = new THREE.Mesh(geometry, material);
     
     this.light = new THREE.PointLight(0xffffff, 2, 0, 0);
